faefea1a53b6025547fb653ec1ebe31fe76edcca
sunarko.sunarko <sunarko.sunarko@aux.gojek.com>
2024-11-15 13:58:37 +0700

    update config url

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    use_env_variable: 'DATABASE_URL',
+    url: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
