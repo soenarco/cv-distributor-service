ee95fa6a1ed7fdcd5fc2676c9e039e12bccc897a
sunarko.sunarko <sunarko.sunarko@aux.gojek.com>
2024-11-15 14:21:34 +0700

    update package

--- config/config.js
+++ config/config.js
@@ -11,4 +11,14 @@ module.exports = {
       }
     }
   },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
+  }
 };
