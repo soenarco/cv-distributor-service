fc45ec8798f04a2a9403481dd99431264ec3f756
sunarko.sunarko <sunarko.sunarko@aux.gojek.com>
2024-11-20 10:00:21 +0700

    allow cors

--- index.js
+++ index.js
@@ -16,6 +16,14 @@ const corsOptions = {
     credentials: true,
 };
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://cv-distributor-project.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.header('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
