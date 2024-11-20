30cb82417d94e64a1724daed1c7b6822e4326615
sunarko.sunarko <sunarko.sunarko@aux.gojek.com>
2024-11-20 09:53:03 +0700

    add cors

--- index.js
+++ index.js
@@ -2,10 +2,21 @@ require('pg');
 require('dotenv').config();
 const express = require('express');
 const multer = require('multer');
+const cors = require('cors');
 const app = express();
 const upload = multer();
 const authRoutes = require('./routes/auth');
 const productRoute = require('./routes/productRoute');
+
+// Konfigurasi CORS
+const corsOptions = {
+    origin: ['https://cv-distributor-project.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'], 
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -13,6 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
     res.send('Hello, CV Distributor Service!');
 });
+
 app.use('/auth', upload.none(), authRoutes);
 app.use('/v1/product', upload.none(), productRoute);
 
